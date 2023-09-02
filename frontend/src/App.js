8878d4d4c99d1b4edbc6af755d37e210be0cfe89
aureliussimon <19101471@students.latrobe.edu.au>
2023-09-02 22:31:49 +1000

    adding button.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,15 @@ import { useState, useEffect } from 'react';  // import useEffect
 import './App.css';
 
 function App() {
+    const [contact, setContact] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost/api/contact')
+        .then(response => response.json())
+        .then(data => setContact)
+    })
+
+
 
     return (
 
@@ -12,6 +21,7 @@ function App() {
             <div class="square">
                 <h1 class="centred-heading">Contact</h1>
                 <input type="text" placeholder='Enter Contact name here'></input>
+                <button class="blue-button">Create Contact</button>
             </div>
             <p class="centred-heading">Click a contact to view associated phone numbers</p>
             
