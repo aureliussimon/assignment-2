44f2a6d3fb5a9761a5dc234f61e6bafa38576ac3
aureliussimon <19101471@students.latrobe.edu.au>
2023-09-02 21:55:23 +1000

    Added text field.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,32 +3,18 @@ import './App.css';
 
 function App() {
 
-    const [phones, setPhones] = useState([]);
-
-    useEffect(() => {
-        fetch('http://localhost/api/phones')
-            .then(response => response.json())
-            .then(data => setPhones(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    },   []);
-
-    useEffect(() => {
-        fetch('http://localhost/api/contacts')
-            .then(response => response.json())
-            .then(data => setContacts(data))
-            .catch((error) => {
-                console.error('Error:', error);
-            });
-    },   []);
-    
-
     return (
-        <div>     
+
+        <div class="body">     
             <h1 class="centred-heading">Contactor</h1>
+            
+            
+            <div class="square">
+                <h1 class="centred-heading">Contact</h1>
+                <input type="text" placeholder='Enter Contact name here'></input>
+            </div>
             <p class="centred-heading">Click a contact to view associated phone numbers</p>
-            <List heading="Contact" contacts={contacts} setContacts={setContacts}/>
+            
         </div>
     );
 }
