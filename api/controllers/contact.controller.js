3098d160dca574b90026676eb2307bfd05687c06
aureliussimon <19101471@students.latrobe.edu.au>
2023-09-02 15:56:28 +1000

    Update to get all contacts.

--- api/controllers/contact.controller.js
+++ api/controllers/contact.controller.js
@@ -23,7 +23,19 @@ exports.create = (req, res) => {
 
 // Get all contacts
 exports.findAll = (req, res) => {
+    Phones.findAll({
+        attributes: { exclude: ['number']}
+    })
     
+        .then((contacts) => {
+            res.status(200).json(contacts);
+        })
+        .catch((err) => {
+            res.status(500).json({
+                message: "Error when retrieving contacts.",
+                error: err.message
+            });
+        });
 };
 
 // Get one contact by id
