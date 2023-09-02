ae7eab8806a51cc355de201f0a10dbed0e706474
aureliussimon <19101471@students.latrobe.edu.au>
2023-09-02 17:36:08 +1000

    update add centre styling.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from 'react';  // import useEffect
 import './App.css';
 
 function App() {
+    
 
     return (
         <div>     
-            <h1>Contactor</h1>
+            <h1 class="centred-heading">Contactor</h1>
+            <p class="centred-heading">Click a contact to view associated phone numbers</p>
         </div>
     );
 }
